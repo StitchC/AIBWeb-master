19ea561a2cd994a5b12c75adb3b76ec4eadf66b8
StitchC <979987824@qq.com>
2017-04-04 12:42:49 +0800

    注册页脚本

---
+++ js/register.js
@@ -0,0 +1,91 @@
+(function checkInput(){
+	//定义私有变量 储存验证提示信息
+	//键名和元素id 关联
+	var hints = {
+		tname:{hint:"请填写教师本人的姓名",correct:"输入正确",error:"输入不正确",reg:/[\S\u4E00-\u9FA5\uF900-\uFA2D]{2,5}/},
+		account:{hint:"必填，长度为4~16位字符",correct:"输入正确",error:"输入不正确",
+		reg:/[\S\w\d]{4,16}/},
+		password:{hint:"必填，长度为4~16位字符,包含字母和数字",correct:"输入正确",error:"输入不正确",
+		reg:/[\d\w]{4,16}/},
+		confirmpwd:{hint:"必须和密码一致",correct:"输入正确",error:"输入不正确"},
+		mail:{hint:"请填写正确的邮箱地址",correct:"输入正确",error:"输入不正确",reg:/^([\d\w]+[_|\_|\.]?)*[\d\w]+@([\d\w]+[_|\_|\.]?)*[\d\w]+\.[\w]{2,3}/}
+
+	};
+
+	//获取所有input 元素
+	var inputWrap = document.getElementById('all-input');
+	var inputs = inputWrap.getElementsByTagName('input');
+
+	//获取提交按钮
+	var completeBtn = document.getElementById('complete-reg');
+
+	//为input元素添加事件
+	for (var i = 0; i < inputs.length; i++) {
+		//为所有input 标签添加是否正确属性
+		inputs[i].isCorrect = false;
+		inputs[i].onfocus = function(){
+			//获取父元素以及提示框
+			var father = this.parentNode;
+			var hintContent = father.getElementsByTagName('span')[0];
+			
+			hintContent.innerHTML = hints[this.id]["hint"];
+			hintContent.style.color = "blue";
+			hintContent.style.visibility = "visible";
+		};
+
+		inputs[i].onblur = function(){
+			//获取父元素及提示框
+			var father = this.parentNode;
+			var hintContent = father.getElementsByTagName('span')[0];
+			//获取失焦时输入框的内容
+			var curVal = this.value;
+
+			if (this.id == "confirmpwd") {
+				if (curVal != (document.getElementById('password')).value || curVal == "") {
+					hintContent.innerHTML = hints[this.id]["error"];
+					hintContent.style.color = "red";
+					hintContent.style.visibility = "visible";
+					this.isCorrect = false;
+				}else{
+					hintContent.innerHTML = hints[this.id]["correct"];
+					hintContent.style.color = "green";
+					hintContent.style.visibility = "visible";
+					this.isCorrect = true;
+				}
+			}else{
+				if (!hints[this.id]["reg"].test(curVal)) {
+					hintContent.innerHTML = hints[this.id]["error"];
+					hintContent.style.color = "red";
+					hintContent.style.visibility = "visible";
+					this.isCorrect = false;
+				}else{
+					hintContent.innerHTML = hints[this.id]["correct"];
+					hintContent.style.color = "green";
+					hintContent.style.visibility = "visible";
+					this.isCorrect = true;
+				}
+			}
+			
+			
+
+		}
+
+	}
+
+	//提交按钮点击事件
+	completeBtn.onclick = function(){
+		var count = 0;
+		for (var i = 0; i < inputs.length; i++) {
+			if (inputs[i].isCorrect == true) {
+				count++;
+			}
+		}
+
+		if (count == inputs.length) {
+			alert("提交成功");
+			/*真实要由ajax 发送内容给后台*/
+		}else{
+			alert("提交失败");
+		}
+	}
+}());
