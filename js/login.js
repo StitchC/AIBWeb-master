c2eeac747f1e7677874e49d9f54f4bc6a05ae1ca
StitchC <979987824@qq.com>
2017-04-11 23:23:59 +0800

    登陆页脚本

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 (function checkBrownser(){
-	var agent=navigator.appName 
-	var version=navigator.appVersion.split(";");
-	var trim_Version=version[1].replace(/[ ]/g,"");
+	var agent=navigator.appName //获取浏览器名字
+	var version=navigator.appVersion.split(";"); //获取浏览器详细信息
+	var trim_Version=version[1].replace(/[ ]/g,"");//获取浏览器版本号
 
 	if(agent=="Microsoft Internet Explorer" && (trim_Version=="MSIE7.0" || trim_Version=="MSIE8.0")) { 
 		$("#warning").css("display","block");
